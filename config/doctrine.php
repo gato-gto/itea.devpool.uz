01c3f3bef20adba59246cf12cd45e8f8856f65a1
Ildar Kabirov <gato.gto.13@gmail.com>
2022-02-21 12:31:02 +0500

    test

--- config/doctrine.php
+++ config/doctrine.php
@@ -1,20 +1,21 @@
 <?php
 
-//require_once 'config.php';
+require_once 'config.php';
 
 const DB_PARAMS = array(
-        'driver'   => 'pdo_mysql',
-        'user'     => 'itea_doctrine',
-        'password' => '280SQiZVsAPw7e6o',
-        'dbname'   => 'itea_doctrine',
+    'driver' => 'pdo_mysql',
+    'user' => 'itea_doctrine',
+    'password' => '280SQiZVsAPw7e6o',
+    'dbname' => 'itea_doctrine',
+    'host' => '10.32.3.20',
 );
 
 if (defined('APPS')) {
     $apps = array_map(
-            function ($appName) {
-                return dirname(__DIR__)."/$appName/models";
-            },
-            constant('APPS')
+        function ($appName) {
+            return dirname(__DIR__) . "/$appName/models";
+        },
+        constant('APPS')
     );
 }
 define('MODELS_PATHS', $apps ?? []);
