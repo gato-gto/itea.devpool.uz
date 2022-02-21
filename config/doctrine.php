8782702035d0a2c33ee66168abfec903f1646e90
Ildar Kabirov <gato.gto.13@gmail.com>
2022-02-21 12:37:34 +0500

    test

--- config/doctrine.php
+++ config/doctrine.php
@@ -3,19 +3,18 @@
 require_once 'config.php';
 
 const DB_PARAMS = array(
-    'driver' => 'driver',
-    'user' => 'user',
-    'password' => 'password',
-    'dbname' => 'dbname',
-    'host' => 'host',
+        'driver'   => 'driver',
+        'user'     => 'user',
+        'password' => 'password',
+        'dbname'   => 'dbname',
 );
 
 if (defined('APPS')) {
     $apps = array_map(
-        function ($appName) {
-            return dirname(__DIR__) . "/$appName/models";
-        },
-        constant('APPS')
+            function ($appName) {
+                return dirname(__DIR__)."/$appName/models";
+            },
+            constant('APPS')
     );
 }
 define('MODELS_PATHS', $apps ?? []);
